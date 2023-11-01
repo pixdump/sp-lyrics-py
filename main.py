4082d5879633dbc7985662c1d60a8bcb8ad2afc6
Parakram <parakram@keemail.me>
2023-11-01 16:06:17 +0545

    not sure why i did but i did

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from base64 import b64encode
 import os
 import json
-from regex_helper import track_id_from_url, album_id_from_url  # noqa
+from typing import Tuple, Union  # noqa
+from regex_helper import track_id_from_url, is_valid_type  # noqa
 
 load_dotenv()
 
@@ -13,7 +14,6 @@ LYRICS_API = str(os.getenv("LYRICS_API"))
 
 
 def get_token() -> str:
-
     auth_string = (CLIENT_ID + ":" + CLIENT_SECRET).encode("utf-8")
     auth_base64 = str(b64encode(auth_string), "utf-8")
 
@@ -28,8 +28,6 @@ def get_token() -> str:
 
     if result.status_code != 200:
         raise SystemExit("Something Went Wrong while getting access token")
-        # print("Something Went Wrong while getting access token")
-        # exit(0)
 
     json_result = json.loads(result.content)
     token = json_result["access_token"]
@@ -40,7 +38,7 @@ def get_auth_header(token: str) -> dict:
     return {"authorization": "Bearer " + token}
 
 
-def get_track_info(token: str, track_id: str) -> tuple[bool, dict]:
+def get_track_info(token: str, track_id: str) -> Tuple[bool, dict]:
     url = f"https://api.spotify.com/v1/tracks/{track_id}"
     headers = get_auth_header(token)
     res = get(url, headers=headers)
@@ -50,7 +48,7 @@ def get_track_info(token: str, track_id: str) -> tuple[bool, dict]:
     return True, track_data
 
 
-def get_lyrics(track_id: str) -> tuple[bool, str]:
+def get_lyrics(track_id: str) -> Tuple[bool, str]:
     url = LYRICS_API + f"{track_id}&format=lrc"
     data = get(url)
     if data.status_code != 200:
@@ -74,19 +72,36 @@ def convert_to_lrc(lyrics_data: dict) -> str:
     return '\n'.join(lrc_lines)
 
 
-def write_to_file(lyrics: str, track_name: str):
+def write_to_file(lyrics: str, track_name: str) -> None:
     with open(f"{track_name}.lrc", 'w', encoding='utf-8') as lrc_file:
         lrc_file.write(lyrics)
 
 
-token = get_token()
-spo = str(input("Enter Spotify URl: "))
-track_id = str(track_id_from_url(spo))
-success, track_info = get_track_info(token, track_id)
-if success:
-    print(f"Track Name: {track_info['name']}")
-    print(f"Album: {track_info['album']['name']}")
-    print(f"Artist: {track_info['artists'][0]['name']}")
-    lyrics_available, lyrics = get_lyrics(track_id)
-    if lyrics_available:
-        write_to_file(lyrics, track_info['name'])
+def input_process_url() -> Tuple[str, str]:
+    while True:
+        url = input("Enter a Spotify URL: ")
+        url_type = is_valid_type(url)
+        if url_type == "Invalid":
+            print("Invalid Spotify URL. Please try again.")
+        else:
+            return url_type, url
+
+
+def main() -> None:
+    token = get_token()
+    url_type, spotify_url = input_process_url()
+    if url_type.lower() == "track":
+        track_id = str(track_id_from_url(spotify_url))
+        success, track_info = get_track_info(token, track_id)
+        if success:
+            print(f"Track Name: {track_info['name']}")
+            print(f"Album: {track_info['album']['name']}")
+            print(f"Artist: {track_info['artists'][0]['name']}")
+            lyrics_available, lyrics = get_lyrics(track_id)
+            if lyrics_available:
+                write_to_file(lyrics, track_info['name'])
+            else:
+                print("Error Getting Lyrics")
+
+
+main()
--- regex_helper.py
+++ regex_helper.py
@@ -1,17 +1,15 @@
 import re
+from typing import Tuple, Union  # noqa
 
 
-def track_id_from_url(spotify_url: str):
+def track_id_from_url(spotify_url: str) -> Union[str, None]:
     id_pattern = r'/track/(\w+)'
-    match = re.search(id_pattern, spotify_url)  # Use re.search to find the match
+    match = re.search(id_pattern, spotify_url)
 
     if match:
         track_id = str(match.group(1))
         print("Track ID:", track_id)
         return track_id
-    else:
-        print("No match found.")
-        return None
 
 
 def album_id_from_url(spotify_url: str):
@@ -29,26 +27,19 @@ def album_id_from_url(spotify_url: str):
         return None
 
 
-def get_spotify_url_type(url: str):
-    """
-    Args:
-        url (_type_): spotify url to be checked
+def is_valid_type(url: str) -> str:
 
-    Returns:
-        url_type: returns if it is track url or album url
-        id: Track id or album id
-    """
     track_url_pattern = r'https://open\.spotify\.com/track/([a-zA-Z0-9]+)'
     album_url_pattern = r'https://open\.spotify\.com/album/([a-zA-Z0-9]+)'
 
     # Check if the URL matches the track pattern
     track_match = re.match(track_url_pattern, url)
     if track_match:
-        return "Track", track_match.group(1)  # Extract the track ID
+        return "Track"  # Extract the track ID
 
     # Check if the URL matches the album pattern
     album_match = re.match(album_url_pattern, url)
     if album_match:
-        return "Album", album_match.group(1)  # Extract the album ID
+        return "Album"  # Extract the album ID
 
-    return "Invalid", None
+    return "Invalid"
