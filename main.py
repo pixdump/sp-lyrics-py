39d4c31a24468e8fc5ff95518aed341872221a64
Parakram <parakram@keemail.me>
2023-11-06 21:46:09 +0545

    add another class for extra functions

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ class SpotifyHelper:
         self.client_id: str = CLIENT_ID
         self.client_secret: str = CLIENT_SECRET
         self.access_token: str = ""
+        self.helper = Helpers()
 
     def __call__(self) -> None:
         if len(self.access_token) == 0:
-            self.access_token = self.get_token()
+            self.get_token()
 
-    def get_token(self) -> str:
+    def get_token(self) -> None:
         auth_url = "https://accounts.spotify.com/api/token"
         data = {"grant_type": "client_credentials"}
         result = post(auth_url, data=data, auth=(self.client_id, self.client_secret))
@@ -34,8 +35,7 @@ class SpotifyHelper:
         if result.status_code != 200:
             message_box(msg="Something Went Wrong while getting access token")
             exit(0)
-
-        return result.json()['access_token']
+        self.access_token = result.json()['access_token']
 
     def get_auth_header(self) -> dict:
         return {"authorization": "Bearer " + self.access_token}
@@ -43,8 +43,8 @@ class SpotifyHelper:
     def process_track(self, track_id) -> None:
         success, track_info = self.get_track_info(track_id)
         if success:
-            print_track_info(track_info)
-            fetch_and_write_lyrics(track_id, track_info['name'])
+            self.helper.print_track_info(track_info)
+            self.helper.fetch_and_write_lyrics(track_id, track_info['name'])
         else:
             message_box("Error Getting Track Info")
 
@@ -52,6 +52,7 @@ class SpotifyHelper:
         url = f"https://api.spotify.com/v1/tracks/{track_id}"
         res = get(url, headers=self.get_auth_header())
         if res.status_code != 200:
+            print(res.text)
             return False, ERR_DEFAULT
         track_data = res.json()
         return True, track_data
@@ -67,7 +68,7 @@ class SpotifyHelper:
     def process_album(self, album_id: str) -> None:
         success, album_info = self.get_album_tracks(album_id)
         if success:
-            track_names, track_ids = get_tracks_list(album_info)
+            track_names, track_ids = self.helper.get_tracks_list(album_info)
             message_box(f"Found Album tracks:\n{track_names}")
             for track_id in track_ids:
                 sleep(3)
@@ -76,57 +77,56 @@ class SpotifyHelper:
             message_box("Error Getting Album Info")
 
 
-def print_track_info(track_info) -> None:
-    print(f"Track Name: {track_info['name']}")
-    print(f"Album: {track_info['album']['name']}")
-    print(f"Artist: {track_info['artists'][0]['name']}\n")
+class Helpers:
+    def __init__(self) -> None:
+        pass
 
+    def print_track_info(self, track_info: dict) -> None:
+        print(f"Track Name: {track_info['name']}")
+        print(f"Album: {track_info['album']['name']}")
+        print(f"Artist: {track_info['artists'][0]['name']}\n")
 
-def get_tracks_list(data: dict) -> Tuple[str, list]:
-    names, track_ids = [], []
-    for item in data["items"]:
-        names.append(item["name"])
-        track_ids.append(item["id"])
-    names = "\n".join(names)
-    return names, track_ids
+    def get_tracks_list(self, data: dict) -> Tuple[str, list]:
+        names, track_ids = [], []
+        for item in data["items"]:
+            names.append(item["name"])
+            track_ids.append(item["id"])
+        names = "\n".join(names)
+        return names, track_ids
 
+    def convert_to_lrc(self, lyrics_data: dict) -> str:
+        lrc_lines = []
+        if lyrics_data['syncType'].lower() == 'unsynced':
+            for line in lyrics_data['lines']:
+                lrc_lines.append(line['words'])
+        else:
+            for line in lyrics_data['lines']:
+                lrc_lines.append(f"[{line['timeTag']}] {line['words']}")
 
-def get_lyrics(track_id: str) -> Tuple[bool, str]:
-    url = LYRICS_API + f"{track_id}&format=lrc"
-    data = get(url)
-    if data.status_code != 200:
-        return False, "Something Went Wrong"
-    lyrics_data = data.json()
-    if lyrics_data['error']:
-        return False, "Error"
-    lyrics = convert_to_lrc(lyrics_data)
-    return True, lyrics
+        return '\n'.join(lrc_lines)
 
+    def fetch_and_write_lyrics(self, track_id, track_name) -> None:
+        lyrics_available, lyrics = self.get_lyrics(track_id)
+        if lyrics_available:
+            self.write_to_file(lyrics, track_name)
+            message_box(msg=f"Successfully Fetched Lyrics for {track_name}")
+        else:
+            message_box("Error Getting Lyrics")
 
-def convert_to_lrc(lyrics_data: dict) -> str:
-    lrc_lines = []
-    if lyrics_data['syncType'].lower() == 'unsynced':
-        for line in lyrics_data['lines']:
-            lrc_lines.append(line['words'])
-    else:
-        for line in lyrics_data['lines']:
-            lrc_lines.append(f"[{line['timeTag']}] {line['words']}")
+    def get_lyrics(self, track_id: str) -> Tuple[bool, str]:
+        url = LYRICS_API + f"{track_id}&format=lrc"
+        data = get(url)
+        if data.status_code != 200:
+            return False, "Something Went Wrong"
+        lyrics_data = data.json()
+        if lyrics_data['error']:
+            return False, "Error"
+        lyrics = self.convert_to_lrc(lyrics_data)
+        return True, lyrics
 
-    return '\n'.join(lrc_lines)
-
-
-def fetch_and_write_lyrics(track_id, track_name) -> None:
-    lyrics_available, lyrics = get_lyrics(track_id)
-    if lyrics_available:
-        write_to_file(lyrics, track_name)
-        message_box(msg=f"Successfully Fetched Lyrics for {track_name}")
-    else:
-        message_box("Error Getting Lyrics")
-
-
-def write_to_file(lyrics: str, track_name: str) -> None:
-    with open(f"{track_name}.lrc", 'w', encoding='utf-8') as lrc_file:
-        lrc_file.write(lyrics)
+    def write_to_file(self, lyrics: str, track_name: str) -> None:
+        with open(f"{track_name}.lrc", 'w', encoding='utf-8') as lrc_file:
+            lrc_file.write(lyrics)
 
 
 def input_dialog_box() -> Tuple[str, str]:
@@ -160,6 +160,7 @@ def main() -> None:
         exit(1)
     url_type, id_from_url = input_dialog_box()
     spotify = SpotifyHelper()
+    spotify.get_token()
     if url_type == "track":
         spotify.process_track(id_from_url)
     elif url_type == "album":
